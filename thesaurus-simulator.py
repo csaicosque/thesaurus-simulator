03a0cd0f40fb7c7f3b7fb89407d8c8293d35e087
Cindi Saicosque <cindipsaicosque@hotmail.com>
2023-01-09 19:56:56 -0300

    Refactor code to use dictionaries

--- thesaurus-simulator.py
+++ thesaurus-simulator.py
@@ -1,53 +1,87 @@
-# Nomes: Cindi Saicosque; Felippe Carrion; Laura Pinzon
-dictionary = [['carro', 'car'], ['maça', 'apple'], ['rato', 'mouse'],]
 
-def registration():
+dictionary = {'Carro': 'Car', 'Maçã': 'Apple', 'Rato': 'Mouse'}
+
+def register():
+    global dictionary
+
     print('1 - Register new word;')
     english = input('Enter the word in english: ')
     portuguese = input('Enter the word in portuguese: ')
-    registration = [portuguese, english]
-    dictionary.append(registration)
+    dictionary[portuguese] = english
+
 
 def remove():
+    global dictionary
+
     print('2 - Remove word;')
-    clear = input('Type the english word you want remover: ')
+    clear = input('Type the english word you want to remove: ') #digita o valor, mas em dicionários só pode ser retirado pela chave
+
     found = False
-    for pair in dictionary:
-        if clear in pair:
+    for k, v in dictionary.items(): #procura pelas chaves
+        if v.upper() == clear.upper(): #quando encontrar a chave que o usuario quer remover
+            dictionary.pop(k) #deleta do dicionário :)
+            print('Removal successful')
             found = True
             break
-    if found:
-        dictionary.remove(pair)
-    else:
-        print('The chosen word is not in the dictionary.')
+
+    if not found: #se não encontrar da um break só pq eu não sabia o que fazer, mas acho bom dizer "queridx essa chave não tem aqui"
+        print('This word doesn\'t exist in this dictionary, please try again')
+
 
 def show():
-    print('3 - Show dictionary;')
-    print('Dictionary:', dictionary)
+    global dictionary
 
-def translate():
+    print('3 - Show dictionary;')
+    print('Choose an option:\n[A] See the whole dictionary\n[B] See only the portuquese words\n[C] See only the english words')
+    show = input('Enter the chosen letter: ').upper()
+    if show == 'A':
+        print(f'This is your whole dictionary: {dictionary}')
+    elif show == 'B':
+        print(f'These are just the Portuguese words in your dictionary: {dictionary.keys()}')
+    elif show == 'C':
+        print(f'These are just the English words in your dictionary: {dictionary.values()}')
+    else:
+        print(f'Sorry, I didn\'t undertend your choice, please try again')
+
+
+def translate(): # Essa parte eu AINDA não sei como fazer usando o objeto dicionário
+    global dictionary
+
     print('4 - Translate;')
+
+    inverted_dictionary = {v.upper(): k for k, v in dictionary.items()}
+
     phrase_usr = input('Enter the phrase you want to translate: ')
     phrase = phrase_usr.split(' ')
     translated = ''
     for word in phrase:
-        for pair in dictionary:
-            if word in pair:
-                translated += pair[0]
-                translated += ' '
-                break
-    print(translated)
+        user_fmt = format_word(word)
+        translated += user_fmt(inverted_dictionary[word.upper()]) + ' '
+    print(f'This is your translated phrase: {translated}')
 
-def exit():
+
+def format_word(word):
+    if word.isupper():
+        user_fmt = str.upper
+    elif word.islower():
+        user_fmt = str.lower
+    elif word.istitle():
+        user_fmt = str.title
+    else:
+        user_fmt = lambda s: s
+    return user_fmt
+
+
+def quit():
     print('5 - Exit;')
 
 
 while True:
-    print('choose an option:')
-    print('[1] Registration\n[2] Remove\n[3] Show \n[4] Translate \n[5] Exit')
+    print('Choose an option:')
+    print('[1] Register\n[2] Remove\n[3] Show\n[4] Translate\n[5] Exit')
     choice = input('Enter the chosen number: ')
     if choice == '1':
-        registration()
+        register()
     elif choice == '2':
         remove()
     elif choice == '3':
@@ -57,5 +91,4 @@ while True:
     elif choice == '5':
         break
     else:
-        print('Invalid option, choose again:')
-        continue
+        print('Invalid option. Please, choose again')
